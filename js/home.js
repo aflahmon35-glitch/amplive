378f0c322a3a67217b0719f62af2449b1d0680b7
aflahmon35-glitch <aflahmon35@gmail.com>
2026-06-29 16:10:01 +0530

    Update home.js

--- js/home.js
+++ js/home.js
@@ -38,62 +38,6 @@ document.addEventListener("DOMContentLoaded", () => {
     ------------------------- */
 
     const matchesContainer = document.getElementById("matchesContainer");
-   /* -------------------------
-   TEAM FLAG MAP
-------------------------- */
-
-const FLAG_MAP = {
-
-    "Algeria": "dz.png",
-    "Argentina": "ar.png",
-    "Australia": "au.png",
-    "Austria": "at.png",
-    "Belgium": "be.png",
-    "Bosnia and Herzegovina": "ba.png",
-    "Brazil": "br.png",
-    "Cabo Verde": "cv.png",
-    "Canada": "ca.png",
-    "Colombia": "co.png",
-    "Croatia": "hr.png",
-    "Curaçao": "cw.png",
-    "Czechia": "cz.png",
-    "DR Congo": "cd.png",
-    "Ecuador": "ec.png",
-    "Egypt": "eg.png",
-    "England": "gb.png",
-    "France": "fr.png",
-    "Germany": "de.png",
-    "Ghana": "gh.png",
-    "Haiti": "ht.png",
-    "Iran": "ir.png",
-    "Iraq": "iq.png",
-    "Japan": "jp.png",
-    "Jordan": "jo.png",
-    "Mexico": "mx.png",
-    "Morocco": "ma.png",
-    "Netherlands": "nl.png",
-    "New Zealand": "nz.png",
-    "Norway": "no.png",
-    "Panama": "pa.png",
-    "Paraguay": "py.png",
-    "Portugal": "pt.png",
-    "Qatar": "qa.png",
-    "Saudi Arabia": "sa.png",
-    "Scotland": "gb-sct.png",
-    "Senegal": "sn.png",
-    "South Africa": "za.png",
-    "South Korea": "kr.png",
-    "Spain": "es.png",
-    "Sweden": "se.png",
-    "Switzerland": "ch.png",
-    "Tunisia": "tn.png",
-    "Türkiye": "tr.png",
-    "USA": "us.png",
-    "United States": "us.png",
-    "Uruguay": "uy.png",
-    "Uzbekistan": "uz.png"
-
-};
 
 
        /* -------------------------
@@ -132,7 +76,7 @@ async function loadMatches(day) {
                 <div class="match-content">
 
                     <div class="team">
-                        <img src="images/logos/team.png">
+                        <img src="images/flags/${FLAG_MAP[match.teamA]}" class="team-logo" alt="${match.teamA}">
                         <span>${match.teamA}</span>
                     </div>
 
@@ -142,7 +86,7 @@ async function loadMatches(day) {
                     </div>
 
                     <div class="team">
-                        <img src="images/logos/team.png">
+                        <img src="images/flags/${FLAG_MAP[match.teamB]}" class="team-logo" alt="${match.teamB}">
                         <span>${match.teamB}</span>
                     </div>
 
